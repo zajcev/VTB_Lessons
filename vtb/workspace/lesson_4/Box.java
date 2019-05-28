9a1273a583dacbaf79bf94a4cd700d97241faa6d
zajcev <winer4ever@yandex.ru>
2019-05-28 10:52:43 +0300

    Исправления

--- vtb/workspace/lesson_4/Box.java
+++ vtb/workspace/lesson_4/Box.java
@@ -4,30 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Box<T extends Fruit> {
-    private String type;
-    private List<T> place = new ArrayList<>();
+    T obj;
+    private List<T> place;
 
+    public Box(T obj){
+        this.obj = obj;
+        this.place = new ArrayList<>();
+    }
 
-    public void put(T obj) {
-        if (place.isEmpty()) type = obj.getType();
-        if (type == obj.getType()) place.add(obj);
-        else System.out.println("В коробке другие фрукты");
+    public void put(T obj){
+        place.add(obj);
     }
 
     public int getWeight() {
-        if (place.isEmpty()) return 0;
         return place.get(0).getWeight() * place.size();
     }
 
-    public boolean compare(Box box) {
+    public boolean compare(Box box){
         return this.getWeight() == box.getWeight();
     }
 
-    public void replace(Box other) {
-        if (type == other.type) {
-            other.place.addAll(place);
-            place.clear();
-        } else
-            System.out.println("Яблоки с апельсинками мешать нельзя");
+    public void replace(Box <? super T> other) {
+                other.place.addAll(place);
+                place.clear();
     }
 }
--- vtb/workspace/lesson_4/MainClass.java
+++ vtb/workspace/lesson_4/MainClass.java
@@ -2,30 +2,15 @@ package vtb.workspace.lesson_4;
 
 public class MainClass {
     public static void main(String[] args) {
-        Box<Fruit> appleBox = new Box<>();
-        Box<Fruit> appleBox2 = new Box<>();
-        Box<Fruit> orangeBox = new Box<>();
+        Box<Apple> appleBox = new Box<>(new Apple());
+        Box<Apple> appleBox2 = new Box<>(new Apple());
+        Box<Orange> orangeBox = new Box<>(new Orange());
 
-        for (int i = 0; i < 5; i++) {
-            appleBox.put(new Apple());
-        }
-        for (int i = 0; i < 5; i++) {
-            appleBox2.put(new Apple());
-        }
-        for (int i = 0; i < 5; i++) {
-            orangeBox.put(new Orange());
-        }
-
-        appleBox.compare(appleBox2);
-        orangeBox.compare(appleBox2);
-        appleBox.replace(orangeBox);
-        appleBox2.replace(appleBox);
-        System.out.println(appleBox.getWeight());
-        System.out.println(appleBox2.getWeight());
+        appleBox.put(new Apple());
+        appleBox2.put(new Apple());
         appleBox.replace(appleBox2);
-        System.out.println(appleBox.getWeight());
-        System.out.println(appleBox2.getWeight());
-        orangeBox.compare(appleBox2);
+        System.out.println(appleBox.compare(orangeBox));
+        System.out.println(appleBox.compare(appleBox2));
 
     }
 }
