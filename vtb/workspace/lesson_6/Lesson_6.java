8dd7b13c5a5be06d9adc5b6c380ff105327c3b70
zajcev <winer4ever@yandex.ru>
2019-05-28 17:14:11 +0300

    Решение дз 6-ого урока

---
+++ vtb/workspace/lesson_6/Lesson_6.java
@@ -0,0 +1,61 @@
+package vtb.workspace.lesson_6;
+
+/**
+ * @author Mikhail Zaitsev
+ */
+public class Lesson_6 {
+    private static final int SIZE = 1000000;
+    private static final int HALF = SIZE / 2;
+    private static float[] arr = new float[SIZE];
+
+    public static void main(String[] args) {
+        createArray();
+        withThreads();
+    }
+
+    private static void createArray(){
+
+        for (int i = 0; i < SIZE ; i++) {
+            arr[i] = 1;
+        }
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < SIZE ; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println(System.currentTimeMillis() - a);
+    }
+    private static void withThreads(){
+        float[] a1 = new float[HALF];
+        float[] a2 = new float[HALF];
+
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < SIZE ; i++) {
+            arr[i] = 1;
+        }
+        System.arraycopy(arr,0,a1,0,HALF);
+        System.arraycopy(arr,HALF,a2,0,HALF);
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < HALF; i++) {
+                a1[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < HALF; i++) {
+                a2[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.arraycopy(a1,0,arr,0,HALF);
+        System.arraycopy(a2,0,arr,HALF,HALF);
+        System.out.println("В двух потоках "+(System.currentTimeMillis() - a));
+    }
+}
+
