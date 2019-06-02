79f6a746f2c5193dad0f12f2f5de85ca7ce1b92c
zajcev <winer4ever@yandex.ru>
2019-06-02 11:56:54 +0300

    Добавлено решение 1-ого пункта. Исправил сортировку в пункте 3.

--- vtb/workspace/lesson_8/Lesson_8.java
+++ vtb/workspace/lesson_8/Lesson_8.java
@@ -1,25 +1,30 @@
 package vtb.workspace.lesson_8;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static java.util.Map.*;
 
 /**
  * @author Mikhail Zaitsev
  */
 public class Lesson_8 {
     public static void main(String[] args) {
-        String[] words = {"one", "two", "three", "one", "two", "four", "one", "five", "two", "four"}; // Доделать
+        String[] words = {"one", "two", "three", "one", "two", "four", "five", "two", "four","four","four","four","four","four","four"};
 
         Worker[] workers = {new Worker("bob", 25, 5000),
-                new Worker("carl", 20, 35000),
-                new Worker("john", 30, 7500),
-                new Worker("james", 33, 80000),
-                new Worker("bill", 45, 60000),
-                new Worker("rodrigo", 50, 50000),
-                new Worker("pavel", 31, 30000)
+                            new Worker("carl", 20, 35000),
+                            new Worker("john", 30, 7500),
+                            new Worker("james", 33, 80000),
+                            new Worker("bill", 45, 60000),
+                            new Worker("rodrigo", 50, 50000),
+                            new Worker("pavel", 31, 30000)
         };
+
+        oftenMeet(words);
         averageSalary(workers);
-        olderWorkers(workers,2);
+        olderWorkers(workers,3);
     }
 
     private static void averageSalary(Worker[] workers) {
@@ -27,18 +32,27 @@ public class Lesson_8 {
                 .mapToInt(Worker::getSalary)
                 .average()
                 .getAsDouble();
-        System.out.println((int)averageSalary);
+        System.out.println("Средняя зарплата "+(int)averageSalary);
     }
 
-    private static void olderWorkers(Worker[] workers, int number) {
+    private static void olderWorkers(Worker[] workers, int countOfWorkers) {
         String names = Arrays.stream(workers)
-                .sorted(Comparator.comparingInt(a -> a.age))
+                .sorted((a1,a2)-> a2.age - a1.age)
                 .map(worker -> worker.name)
-                .skip(workers.length - number)
-                .limit(number)
+                .limit(countOfWorkers)
                 .collect(Collectors.joining(","));
-        System.out.println(number+" самых старших сотрудника зовут: "+names);
+        System.out.println(countOfWorkers+" самых старших сотрудника зовут: "+names);
 
     }
 
+    private static void oftenMeet(String[] words){
+        String map = Arrays.stream(words)
+                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting())) // (word -> word, количество добавлений)
+                .entrySet()
+                .stream()
+                .max(Comparator.comparing(Entry::getValue))
+                .map(Entry::getKey)
+                .get();
+        System.out.println("Самое часто встречающееся слово " + map);
+    }
 }
